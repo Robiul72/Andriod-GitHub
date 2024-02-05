9936fa7b6b73b05ded8a6e2a20045fe38b1e5540
Robiul72 <robiulislam366972@gmail.com>
2024-02-06 00:57:07 +0600

    add
    06-02-2024

---
+++ Andriod Project/app/src/main/java/com/andriod/andriodproject/Contact.java
@@ -0,0 +1,33 @@
+package com.andriod.andriodproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Contact extends AppCompatActivity {
+
+    private Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+
+        btnSubmit=findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Contact.this, Home.class);
+
+                startActivity(intent);
+            }
+        });
+
+    }
+}
